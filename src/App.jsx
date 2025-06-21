f6a3487c107d60462706adc52868c7507d2a1935
vijaya-attaluri <150883937+vijaya-attaluri@users.noreply.github.com>
2025-06-21 16:05:43 +0530

    app.jsx change

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-     
-     
-     
-     <p>Welcome to react project</p>
+    <div className="App">  
+     <p>Welcome to react project...</p>
     </div>
   )
 }
