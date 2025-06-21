ceb93fe440b02242954dfe6ce1b98706d6a83ba2
vijaya-attaluri <150883937+vijaya-attaluri@users.noreply.github.com>
2025-06-21 16:01:47 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     
+     
+     
+     <p>Welcome to react project</p>
+    </div>
+  )
+}
+
+export default App
